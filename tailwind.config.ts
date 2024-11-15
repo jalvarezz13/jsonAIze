1cc81c4b295d70e747a40a77fc9b7643b6c564d8
Javier Álvarez <javier.alvarezparamo@telefonica.com>
2024-11-15 15:26:32 +0100

    format files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
-import {nextui} from '@nextui-org/theme';
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
+import { nextui } from "@nextui-org/theme"
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -18,5 +18,6 @@ const config: Config = {
   },
   darkMode: "class",
   plugins: [nextui()],
-};
-export default config;
+}
+
+export default config
